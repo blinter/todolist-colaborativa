1a1d217aeed83a6a9056be52e3db0c2fdf50b685
Symon Lopes <symon.lopes@nwaypro.com>
2021-02-14 18:07:38 -0300

    Prototype server.

--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.listen(4500, () => {
-  console.log(`Example app listening`)
+const db = new Map()
+
+app.listen(4500, () => { })
+
+app.post('/list', (req, res) => {
+  console.log(req.body)
+  db.set(req.body.key, req.body)
+  res.status(200).send()
+  console.log(db)
 })
 
-app.get('/list', async (req, res) => {
-  res.status(200).send()
+app.get('/list/:key', (req, res) => {
+  res.status(200).send(db.get(req.params.key))
 })
